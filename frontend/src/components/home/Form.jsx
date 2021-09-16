7f964b8f648ac7e3feaf89d20705b80a76b2f84b
yemmyharry <yemmyharry@gmail.com>
2021-09-17 00:16:33 +0100

    cleaned up form component

--- frontend/src/components/home/Form.jsx
+++ frontend/src/components/home/Form.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import axios from "axios"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,24 +13,19 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ValidationTextFields() {
+
+
+const ValidationTextFields = () => {
   const classes = useStyles();
-
-  const clickHandler = () => {
-    axios.get("http://localhost:3000/").then(res => {
-        console.log(res.data.data[0])
-    }).catch(err => {
-        console.log(err)
-    })
-    }
-
   return (
-    <form className={classes.root} noValidate autoComplete="off">
+      
+
+    <form className={classes.root} noValidate autoComplete="off" >
         <Typography variant="h4" gutterBottom>
         Create a bank
       </Typography>
       <div>
-        <TextField id="standard-error" label="Bank Name" defaultValue="Bank Of America" />
+        <TextField id="name" label="Bank Name" name="name" defaultValue="Bank of America" />
         <TextField id="standard-error" label="Interest (%)" defaultValue="2" />
       </div>
       <div>
@@ -42,9 +36,11 @@ export default function ValidationTextFields() {
         <TextField id="standard-error" label="Loan Term (No. of months)" defaultValue="5" />
       
       </div>
-      <Button variant="contained" color="primary" onClick={clickHandler}>
+      <Button variant="contained" color="primary">
         CREATE
       </Button>
     </form>
   );
 }
+
+export default ValidationTextFields
