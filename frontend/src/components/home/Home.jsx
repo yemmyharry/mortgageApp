e4a0f934d7d3777f9fed7a555b2bd2faac1168b5
yemmyharry <yemmyharry@gmail.com>
2021-09-17 00:18:29 +0100

    cleaned up home

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import NavBar from './NavBar'
+// import SimpleBottomNavigation from './Footer'
+import CenteredGrid from './Grid';
 
 
 export default function App() {
   return (
     <div>
     <NavBar/>
-    
+    <CenteredGrid/>
+    {/* <SimpleBottomNavigation/> */}
     </div>
   
   );
