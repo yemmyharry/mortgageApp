b1f21c512863e0bbc6799af4510eba54f711e7c7
yemmyharry <yemmyharry@gmail.com>
2021-09-17 17:15:12 +0100

    implemented routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-import Home from './components/home/Home.jsx'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import CalcPage from "./components/calcpage/CalcPage";
+import Home from './components/home/Home'
+import NavBar from './components/home/NavBar'
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Home/>
-    </div>
+    <NavBar/>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/calculate">
+          <CalcPage/>
+        </Route>
+      </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
