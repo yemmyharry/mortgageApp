0b1d9007d1ffe9476830a972a22d59ec899270bc
yemmyharry <yemmyharry@gmail.com>
2021-09-17 17:13:57 +0100

    added router

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import NavBar from './NavBar'
+import {Link} from "react-router-dom"
 // import SimpleBottomNavigation from './Footer'
 import CenteredGrid from './Grid';
 
 
 export default function App() {
   return (
-    <div>
-    <NavBar/>
+    <Link to="/">
     <CenteredGrid/>
     {/* <SimpleBottomNavigation/> */}
-    </div>
+    </Link>
   
   );
 }
